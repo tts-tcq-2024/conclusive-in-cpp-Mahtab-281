1957a5ed4b46062c2b1b6896db3e615ac8241ed4
Mahtab Ahmad <105929699+Mahtab-281@users.noreply.github.com>
2024-10-30 11:14:31 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,76 +1,49 @@
 #include "typewise-alert.h"
+#include <sstream>
+#include <vector>
+#include <string>
 
-void printStringToConsole(std::string inputString) {
-  std::cout << inputString;
-}
+// Global instance of MessageStore
+MessageStore messageStore;
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
-}
-
-void setLowerAndUpperLimits(int lowerLimitList[] ,int upperLimitList[]) {
-  lowerLimitList[PASSIVE_COOLING] = 0;
-  upperLimitList[PASSIVE_COOLING] = 35;
-  
-  lowerLimitList[HI_ACTIVE_COOLING] = 0;
-  upperLimitList[HI_ACTIVE_COOLING] = 45;
-  
-  lowerLimitList[MED_ACTIVE_COOLING] = 0;
-  upperLimitList[MED_ACTIVE_COOLING] = 40;
+    return (value < lowerLimit) ? TOO_LOW :
+           (value > upperLimit) ? TOO_HIGH :
+           NORMAL;
 }
 
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-  int lowerLimit[noOfCoolingTypes];
-  int upperLimit[noOfCoolingTypes];
-  setLowerAndUpperLimits(lowerLimit,upperLimit);
-  
-  return inferBreach(temperatureInC, lowerLimit[coolingType], upperLimit[coolingType]);
+    int lowerLimit = coolingLimits[coolingType].lowerLimit;
+    int upperLimit = coolingLimits[coolingType].upperLimit;
+
+    return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-std::string getAlertMessage(AlertTarget alertTarget, BreachType breachType) {
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      return (sendToController(breachType));
-      break;
-    case TO_EMAIL:
-      return (sendToEmail(breachType));
-      break;
-  }
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+
+    if (alertTarget == TO_CONTROLLER) {
+        sendToController(breachType);
+    } else {
+        sendToEmail(breachType);
+    }
 }
 
-std::string checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
-  std::string alertMessage = getAlertMessage(alertTarget, breachType);
-  printStringToConsole(alertMessage);
-  return alertMessage;
+void sendToController(BreachType breachType) {
+    const unsigned short header = 0xfeed;
+    std::ostringstream message;
+    message << std::hex << header << " : " << breachType;
+    messageStore.addMessage(message.str());
 }
 
-std::string getControllerMessage(const int header, BreachType breachType) {
-  std::ostringstream headerStringStream;
-  headerStringStream << std::hex << header;
-  return (headerStringStream.str() + " : " + std::to_string(breachType) + "\n");
-}
+void sendToEmail(BreachType breachType) {
+    if (breachType == NORMAL) return;
 
-std::string sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  return (getControllerMessage(header, breachType));
-}
+    const char* recipient = "a.b@c.com";
+    std::string message = (breachType == TOO_LOW) 
+                          ? "Hi, the temperature is too low" 
+                          : "Hi, the temperature is too high";
 
-std::string getEmailMessage(std::string recepient, BreachType breachType) {
-  std::string message[noOfBreachTypes];
-  message[TOO_LOW]  = "To: " +  recepient + "\nHi, the temperature is too low\n";
-  message[TOO_HIGH] = "To: " +  recepient + "\nHi, the temperature is too high\n";
-  message[NORMAL]   = "";
-  return message[breachType];
-}
-
-std::string sendToEmail(BreachType breachType) {
-  const std::string recepient = "a.b@c.com";
-  return (getEmailMessage(recepient, breachType));
+    messageStore.addMessage("To: " + std::string(recipient));
+    messageStore.addMessage(message);
 }
