46c6c690ac9caaf1b727cd0faa29ceaa9402e5d7
Mahtab Ahmad <105929699+Mahtab-281@users.noreply.github.com>
2024-10-30 10:24:32 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -11,6 +14,17 @@ typedef enum {
   TOO_LOW,
   TOO_HIGH
 } BreachType;
+
+typedef struct {
+    int lowerLimit;
+    int upperLimit;
+} CoolingLimits;
+
+const CoolingLimits coolingLimits[] = {
+    {0, 35},  // PASSIVE_COOLING
+    {0, 45},  // HI_ACTIVE_COOLING
+    {0, 40}   // MED_ACTIVE_COOLING
+};
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
@@ -24,6 +38,24 @@ typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
+
+struct MessageStore {
+  std::vector<std::string> messages;
+
+  void addMessage(const std::string& message) {
+    messages.push_back(message);
+  }
+
+  void clearMessages() {
+    messages.clear();
+  }
+
+  std::vector<std::string> getMessages() const {
+    return messages;
+  }
+};
+
+extern MessageStore messageStore;
 
 void checkAndAlert(
   AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
