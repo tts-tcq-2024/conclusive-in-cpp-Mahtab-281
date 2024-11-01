39a968e5a33b9d7c51d73445f449de0cc4db9517
Mahtab Ahmad <105929699+Mahtab-281@users.noreply.github.com>
2024-11-01 21:33:06 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,64 +1,38 @@
 #pragma once
- 
-#include <vector>
+using namespace std;
 #include <string>
- 
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
- 
-typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
-} BreachType;
- 
-typedef struct {
-    int lowerLimit;
-    int upperLimit;
-} CoolingLimits;
- 
-const CoolingLimits coolingLimits[] = {
-    {0, 35},  // PASSIVE_COOLING
-    {0, 45},  // HI_ACTIVE_COOLING
-    {0, 40}   // MED_ACTIVE_COOLING
+
+class TypewiseAlert
+{
+public:
+  enum class CoolingType{
+    PASSIVE_COOLING,
+    HI_ACTIVE_COOLING,
+    MED_ACTIVE_COOLING
+  };
+
+  enum class BreachType{
+    NORMAL,
+    TOO_LOW,
+    TOO_HIGH
+  };
+
+  enum class AlertTarget{
+    TO_CONTROLLER,
+    TO_EMAIL
+  };
+
+  struct BatteryCharacter {
+        CoolingType coolingType;
+        string brand;
+  };
+
+  static BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+  static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+  
+  static void checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+  
+  static void sendToController(BreachType breachType);
+  static void sendToEmail(BreachType breachType);
 };
- 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
- 
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
-} AlertTarget;
- 
-typedef struct {
-  CoolingType coolingType;
-  char brand[48];
-} BatteryCharacter;
- 
-struct MessageStore {
-  std::vector<std::string> messages;
- 
-  void addMessage(const std::string& message) {
-    messages.push_back(message);
-  }
- 
-  void clearMessages() {
-    messages.clear();
-  }
- 
-  std::vector<std::string> getMessages() const {
-    return messages;
-  }
-};
- 
-extern MessageStore messageStore;
- 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
- 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
